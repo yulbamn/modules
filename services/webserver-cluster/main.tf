83df75f7a2e21d6ee90340a29bee6eee8e9b54d0
yulbamn <yulbamn@gmail.com>
2023-02-12 15:01:23 +0900

    second commit of modules repo

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -4,7 +4,7 @@ resource "aws_launch_configuration" "example" {
   security_groups = [aws_security_group.instance.id]
 
   # Render the User Data script as a template
-  user_data = templatefile("user-data.sh", {
+  user_data = templatefile("multi-user-data.sh", {
     server_port = var.server_port
     db_address  = data.terraform_remote_state.db.outputs.address
     db_port     = data.terraform_remote_state.db.outputs.port
